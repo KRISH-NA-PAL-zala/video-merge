1387b5eb1ab6759c05b00723f4edb738da98b28d
brendan.craven <brendan.craven@student.howest.be>
2023-04-17 04:04:17 +0200

    add more functionality, and a GUI

--- video-merge.py
+++ video-merge.py
@@ -1,9 +1,41 @@
 import moviepy.editor as mp
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
 
-video_files = ["video1.mp4", "video2.mp4", "video3.mp4"]
 
-video_clips = [mp.VideoFileClip(video_file) for video_file in video_files]
+def open_file_dialog():
+    file_paths = filedialog.askopenfilenames(title="Select video files", filetypes=[("MP4 files", "*.mp4")])
+    for file_path in file_paths:
+        listbox.insert(tk.END, file_path)
 
-final_video = mp.concatenate_videoclips(video_clips)
 
-final_video.write_videofile("merged_video.mp4", codec="libx264")
+def remove_selected_files():
+    selected_indices = listbox.curselection()
+    listbox.delete(*selected_indices)
+
+
+def merge_videos():
+    file_paths = listbox.get(0, tk.END)
+
+    video_clips = [mp.VideoFileClip(video_file) for video_file in file_paths]
+    final_video = mp.concatenate_videoclips(video_clips)
+    final_video.write_videofile("merged_video.mp4", codec="libx264")
+
+    messagebox.showinfo("Success", "Videos merged successfully!")
+
+
+root = tk.Tk()
+root.title("Video Merger")
+
+listbox = tk.Listbox(root, selectmode=tk.MULTIPLE, width=80)
+listbox.pack(pady=10)
+
+open_button = tk.Button(root, text="Add Videos", command=open_file_dialog)
+open_button.pack(pady=5)
+remove_button = tk.Button(root, text="Remove Selected", command=remove_selected_files)
+remove_button.pack(pady=5)
+merge_button = tk.Button(root, text="Merge Videos", command=merge_videos)
+merge_button.pack(pady=5)
+
+root.mainloop()
